4d206e9d203ff33afd2b84820f214a8096b41b01
HappyKiller <007kostiv@gmail.com>
2020-04-06 10:01:16 +0300

    7 commit

--- webpage/main.js
+++ webpage/main.js
@@ -6,6 +6,7 @@ let ternopil = document.getElementById("ternopil");
 let volyn = document.getElementById("volyn");
 let zakarpattia = document.getElementById("zakarpattia");
 let lviv = document.getElementById("lviv");
+let rivne = document.getElementById("rivne");
 //
 let mask = document.getElementById("mask");
 let modal = document.getElementById("path-modal");
@@ -20,20 +21,29 @@ function pathModal(isModalOn, region) {
         case "chernivci":
           chernivci.style.cssText = "display: block;";
           break;
+
         case "ivanoFrankivsk":
           ivanoFrankivsk.style.cssText = "display: block;";
           break;
+
         case "ternopil":
           ternopil.style.cssText = "display: block;";
           break;
+
         case "volyn":
           volyn.style.cssText = "display: block;";
           break;
+
         case "zakarpattia":
           zakarpattia.style.cssText = "display: block;";
           break;
+
         case "lviv":
           lviv.style.cssText = "display: block;";
+          break;
+
+        case "rivne":
+          rivne.style.cssText = "display: block;";
           break;
       }
       break;
@@ -46,11 +56,12 @@ function pathModal(isModalOn, region) {
       volyn.style.cssText = "display: none;";
       zakarpattia.style.cssText = "display: none;";
       ternopil.style.cssText = "display: none;";
+      rivne.style.cssText = "display: none;";
       break;
   }
 }
 
-document.onkeydown = function(evt) {
+document.onkeydown = function (evt) {
   evt = evt || window.event;
   if (evt.keyCode == 27) {
     mask.style.cssText = "display: none";
@@ -61,11 +72,12 @@ document.onkeydown = function(evt) {
     volyn.style.cssText = "display: none;";
     zakarpattia.style.cssText = "display: none;";
     ternopil.style.cssText = "display: none;";
+    rivne.style.cssText = "display: none;";
   }
 };
 
 // Hide mask
-$("#mask").click(function() {
+$("#mask").click(function () {
   mask.style.cssText = "display: none";
   modal.style.cssText = "display: none";
   lviv.style.cssText = "display: none;";
@@ -74,52 +86,61 @@ $("#mask").click(function() {
   volyn.style.cssText = "display: none;";
   zakarpattia.style.cssText = "display: none;";
   ternopil.style.cssText = "display: none;";
+  rivne.style.cssText = "diplay: none;";
 });
 
 // Pointer1 animation
-$("#pointer1").mouseover(function() {
+$("#pointer1").mouseover(function () {
   $("#Fill-10").css("fill", "#f89924");
 });
-$("#pointer1").mouseout(function() {
+$("#pointer1").mouseout(function () {
   $("#Fill-10").css("fill", "#f8ca24");
 });
 
 // Pointer2 animation
-$("#pointer2").mouseover(function() {
+$("#pointer2").mouseover(function () {
   $("#Fill-26").css("fill", "#f89924");
 });
-$("#pointer2").mouseout(function() {
+$("#pointer2").mouseout(function () {
   $("#Fill-26").css("fill", "#f8ca24");
 });
 
 // Pointer3 animation
-$("#pointer3").mouseover(function() {
+$("#pointer3").mouseover(function () {
   $("#Fill-8").css("fill", "#f89924");
 });
-$("#pointer3").mouseout(function() {
+$("#pointer3").mouseout(function () {
   $("#Fill-8").css("fill", "#f8ca24");
 });
 
 // Pointer4 animation
-$("#pointer4").mouseover(function() {
+$("#pointer4").mouseover(function () {
   $("#Fill-25").css("fill", "#f89924");
 });
-$("#pointer4").mouseout(function() {
+$("#pointer4").mouseout(function () {
   $("#Fill-25").css("fill", "#f8ca24");
 });
 
 // Pointer5 animation
-$("#pointer5").mouseover(function() {
+$("#pointer5").mouseover(function () {
   $("#Fill-4").css("fill", "#f89924");
 });
-$("#pointer5").mouseout(function() {
+$("#pointer5").mouseout(function () {
   $("#Fill-4").css("fill", "#f8ca24");
 });
 
 // Pointer6 animation
-$("#pointer6").mouseover(function() {
+$("#pointer6").mouseover(function () {
   $("#Fill-23").css("fill", "#f89924");
 });
-$("#pointer6").mouseout(function() {
+$("#pointer6").mouseout(function () {
   $("#Fill-23").css("fill", "#f8ca24");
+});
+
+// Pointer7 animation
+$("#pointer7").mouseover(function () {
+  $("#Fill-9").css("fill", "#f89924");
+});
+$("#pointer7").mouseout(function () {
+  $("#Fill-9").css("fill", "#f8ca24");
 });
